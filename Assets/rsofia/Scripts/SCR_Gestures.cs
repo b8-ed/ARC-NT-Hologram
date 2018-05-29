2856f90db8d1fa4674dd9a035f799afdacb5a3d2
Sofia Brown <ruthsofiab@gmail.com>
2018-05-29 12:43:47 -0500

    Animaciones y paquete de leap motion

---
+++ Assets/rsofia/Scripts/SCR_Gestures.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class SCR_Gestures : MonoBehaviour {
+
+    private void Update()
+    {
+        
+    }
+}
